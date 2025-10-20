24012d72a8d1a4497d2e4e640913d2a72a80f9df
Daniel Parrillas <daniel16parrillas@gmail.com>
2025-10-20 11:45:52 -0600

    Agregar lógica de notificación en el layout de la aplicación: implementar notificaciones para éxito, advertencia, información y mensajes.

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,14 +1,36 @@
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from '@inertiajs/react';
+import { useEffect, type ReactNode } from 'react';
+import { toast } from 'sonner';
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
-    </AppLayoutTemplate>
-);
+export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => {
+    const { success, warning, info, message, error } = usePage().props || {};
+
+    useEffect(() => {
+        if (success) toast.success(success as string);
+    }, [success]);
+
+    useEffect(() => {
+        if (warning) toast.warning(warning as string);
+    }, [warning]);
+
+    useEffect(() => {
+        if (info) toast.info(info as string);
+    }, [info]);
+
+    useEffect(() => {
+        if (message) toast.message(message as string);
+    }, [message]);
+
+    return (
+        <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+            {children}
+        </AppLayoutTemplate>
+    );
+};
